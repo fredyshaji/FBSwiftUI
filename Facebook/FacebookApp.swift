6e60a82408534196d44d8e3ad0b41377fe8b8856
Fredy Shaji <fredy.a.ext.shaji@swedbank.se>
2022-12-21 14:47:20 +0100

    no message

---
+++ Facebook/FacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookApp.swift
+//  Facebook
+//
+//  Created by Fredy Shaji on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct FacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(text: .constant(""))
+        }
+    }
+}
